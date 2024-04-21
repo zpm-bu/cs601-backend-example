b1f37cb0c9c2f15182d2dccd12576edda578595d
zpm <iamzacharymoring@gmail.com>
2024-04-20 21:56:57 -0400

    Add basic endpoints and frontend component to render them

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,3 +8,13 @@ CORS(app)
 @app.route("/")
 def homepage():
     return "<p>This is the root.</p>"
+
+
+@app.route("/example")
+def serve_example_json():
+    return jsonify({"firstElem": 1, "secondElem": "Foo"})
+
+
+@app.route("/example/<int:argument>", methods=["GET", "POST"])
+def serve_example_with_argument(argument):
+    return jsonify({"firstElem": argument, "secondElem": "Foo"})
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <></>;
+  const [firstElem, setFirstElem] = useState();
+  const [secondElem, setSecondElem] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:5000/example/12")
+      .then((response) => response.json())
+      .then((json) => {
+        setFirstElem(json.firstElem);
+        setSecondElem(json.secondElem);
+      })
+      .catch((_error) => {});
+  }, []);
+
+  return (
+    <>
+      <ul>
+        <li>{firstElem}</li>
+        <li>{secondElem}</li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
