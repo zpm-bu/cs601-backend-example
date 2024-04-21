46a2e50f565479fd9b5f35ee448203e671510ab0
zpm <iamzacharymoring@gmail.com>
2024-04-21 11:08:16 -0400

    Add example of working POST method

--- backend/server.py
+++ backend/server.py
@@ -1,20 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 
 
+# API Examples =================================================================
 @app.route("/")
 def homepage():
+    # If the return statement is a string, Flask will return it as an HTML
+    # page when you hit the endpoint.
     return "<p>This is the root.</p>"
 
 
 @app.route("/example")
 def serve_example_json():
+    # Use the `jsonify` function to return anything in the form of a JSON
+    # object. Flask can handle encoding a very surprising number of things; if
+    # it can't be encoded, Flask will raise an error.
     return jsonify({"firstElem": 1, "secondElem": "Foo"})
 
 
-@app.route("/example/<int:argument>", methods=["GET", "POST"])
+# To add an argument to the route, use <{type:}argname>. The `type:` at the
+# start is optional, but good practice. Then, pass an argument with the same
+# `argname` into the function, and Flask will automatically handle the binding.
+@app.route("/example/<int:argument>", methods=["GET"])
 def serve_example_with_argument(argument):
     return jsonify({"firstElem": argument, "secondElem": "Foo"})
+
+
+# The Flask `request` object contains the request itself, including both
+# metadata like the method ("GET", "POST", etc.) and the contents of the
+# request. You can use it to bind to API calls like so.
+@app.route("/example/hello", methods=["POST"])
+def create_example_and_log_it():
+    try:
+        data = request.get_json()
+    except:
+        return "Bad req", 400
+
+    name = data.get("name", None)
+    return jsonify({"message": f"Hello, {name}."})
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import { useEffect, useState } from "react";
 
+const POST = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+};
+
+function InputSection() {
+  const [name, setName] = useState("");
+  const [greeting, setGreeting] = useState("");
+
+  function handleClick() {
+    fetch("http://localhost:5001/example/hello", {
+      ...POST,
+      body: JSON.stringify({ name }),
+    })
+      .then((response) => response.json())
+      .then((json) => setGreeting(json.message))
+      .catch();
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name?"
+      />
+      <button onClick={handleClick}>Submit</button>
+      <p>{greeting}</p>
+    </>
+  );
+}
+
 function App() {
   const [firstElem, setFirstElem] = useState();
   const [secondElem, setSecondElem] = useState();
 
   useEffect(() => {
-    fetch("http://localhost:5000/example/12")
+    fetch("http://localhost:5001/example/12")
       .then((response) => response.json())
       .then((json) => {
         setFirstElem(json.firstElem);
@@ -16,10 +49,14 @@ function App() {
 
   return (
     <>
+      <h1>Example of API</h1>
+      <h2>GET example:</h2>
       <ul>
         <li>{firstElem}</li>
         <li>{secondElem}</li>
       </ul>
+      <h2>POST example:</h2>
+      <InputSection />
     </>
   );
 }
